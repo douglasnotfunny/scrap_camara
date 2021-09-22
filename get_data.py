4678967c4d6d460cbb8e89bfc50c16ec6af969fb
douglasnotfunny <contatodouglassiqueira@gmail.com>
2021-09-22 20:28:20 -0300

    teste

---
+++ get_data.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import chromedriver_binary  # Adds chromedriver binary to path
+
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
